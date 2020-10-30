3719f632867e8a3417a693be022fc9d95e7101c6
Santiago <santo1996.29@gmail.com>
2020-10-30 12:03:52 -0400

    feature: update form state on input change

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
-const Form = () => (
-  <>
-    <h2>Crear Cita</h2>
-    <form>
-      <label>Nombre de tu mascota</label>
-      <input type="text" name="pet" className="u-full-width" />
-      <label>Nombre del dueño</label>
-      <input type="text" name="owner" className="u-full-width" />
-      <label>Fecha en la que se da de alta</label>
-      <input type="date" name="date_free" className="u-full-width" />
-      <label>Hora en la que se da de alta</label>
-      <input type="time" name="time_free" className="u-full-width" />
-      <label>Síntomas</label>
-      <textarea className="u-full-width" name="symptoms"></textarea>
-      <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
-    </form>
-  </>
-);
+const Form = () => {
+  const [appointment, setAppointment] = useState({
+    pet: '',
+    owner: '',
+    date_appointment: '',
+    time_appointment: '',
+    symptoms: ''
+  });
+
+  const handleChange = e => {
+    setAppointment({ ...appointment, pet: e.target.value });
+  }
+
+  return (
+    <>
+      <h2>Crear Cita</h2>
+      <form>
+        <label>Nombre de tu mascota</label>
+        <input type="text" name="pet" className="u-full-width" onChange={handleChange} />
+        <label>Nombre del dueño</label>
+        <input type="text" name="owner" className="u-full-width" onChange={handleChange} />
+        <label>Fecha</label>
+        <input type="date" name="date_appointment" className="u-full-width" onChange={handleChange} />
+        <label>Hora</label>
+        <input type="time" name="time_appointment" className="u-full-width" onChange={handleChange} />
+        <label>Síntomas</label>
+        <textarea className="u-full-width" name="symptoms" onChange={handleChange}></textarea>
+        <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
+      </form>
+    </>
+  );
+}
 
 export default Form;
